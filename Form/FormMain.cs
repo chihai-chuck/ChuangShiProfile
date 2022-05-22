4f3884fffc3baa8ad1af7eb7c439b9f327dc4073
Chuck <chuck@chihai.me>
2022-05-22 19:04:02 +0800

    升级.NETFramework版本

--- Form/FormMain.cs
+++ Form/FormMain.cs
@@ -10,17 +10,15 @@ using System.Threading;
 using System.Reflection;
 using System.Resources;
 
-namespace ChuangShiProfile
-{
-    public partial class FormMain : Form
-    {
+namespace ChuangShiProfile {
+    public partial class FormMain : Form {
         private int _requestNum = 0;
         private short _requestPage = 1;
 
         private delegate string FansData(); // 异步请求数据
 
-        public class FansRankJson // 粉丝排行榜JSON实体类
-        {
+        // 粉丝排行榜JSON实体类
+        public class FansRankJson {
             public long uid { get; }
             public string icon { get; }
             public string title { get; }
@@ -29,12 +27,11 @@ namespace ChuangShiProfile
             public int pos { get; }
         }
 
-        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args) // 用于自动关联资源文件中的dll文件（dll文件是项目中使用的第三方库），以让生成exe无需附带dll文件即可正常运行
-        {
+        // 用于自动关联资源文件中的dll文件（dll文件是项目中使用的第三方库），以让生成exe无需附带dll文件即可正常运行
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args) {
             var dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
             dllName = dllName.Replace(".", "_");
-            if (dllName.EndsWith("_resources"))
-            {
+            if (dllName.EndsWith("_resources")) {
                 return null;
             }
             var rm = new ResourceManager(GetType().Namespace + ".Properties.Resources", Assembly.GetExecutingAssembly());
@@ -42,15 +39,13 @@ namespace ChuangShiProfile
             return Assembly.Load(bytes);
         }
 
-        public FormMain()
-        {
+        public FormMain() {
             AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve); // 调用dll关联
             Control.CheckForIllegalCrossThreadCalls = false; // 使线程中可以直接更改winform控件属性
             InitializeComponent();
         }
 
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
+        private void btnSearch_Click(object sender, EventArgs e) {
             if (string.IsNullOrEmpty(textQQ.Text + textNick.Text)) {
                 MessageBox.Show("必须输入QQ号或昵称之一才可进行查询！", "提示");
                 return;
@@ -63,15 +58,13 @@ namespace ChuangShiProfile
             _requestPage = 1;
             _requestNum = 0;
 
-            var findFansDataThread = new Thread(new ThreadStart(FindFansData))
-            {
+            var findFansDataThread = new Thread(new ThreadStart(FindFansData)) {
                 IsBackground = true
             };
             findFansDataThread.Start();
         }
 
-        private void FindFansData()
-        {
+        private void FindFansData() {
             if (_requestPage > 10) {
                 this.Height = 185;
                 btnSearch.Enabled = true;
@@ -90,13 +83,11 @@ namespace ChuangShiProfile
 
             var isDone = false;
 
-            fansChildrenList.ForEach(i =>
-            {
+            fansChildrenList.ForEach(i => {
                 var searchResult = JsonConvert.DeserializeObject<FansRankJson>(i.ToString());
                 _requestNum++;
                 lblNum.Text = _requestNum.ToString();
-                if (searchResult.uid.ToString() != textQQ.Text && searchResult.nick != textNick.Text)
-                {
+                if (searchResult.uid.ToString() != textQQ.Text && searchResult.nick != textNick.Text) {
                     return;
                 }
                 isDone = true;
@@ -106,8 +97,7 @@ namespace ChuangShiProfile
                 lblFans.Text = searchResult.num.ToString();
                 lblRank.Text = searchResult.pos.ToString();
                 var imageStream = WebRequest.Create(searchResult.icon).GetResponse().GetResponseStream();
-                if (imageStream != null)
-                {
+                if (imageStream is null) {
                     pictureCover.Image = Image.FromStream(imageStream);
                 }
                 this.Height = 375;
@@ -116,25 +106,21 @@ namespace ChuangShiProfile
                 textNick.Enabled = true;
                 btnSearch.Text = "查询";
             });
-            if (isDone)
-            {
+            if (isDone) {
                 return;
             }
             _requestPage++;
             FindFansData();
         }
 
-        private string PostFansData()
-        {
+        private string PostFansData() {
             var fansAsync = new FansData(FansRequest);
             var fansResult = fansAsync.BeginInvoke(FansCallBack, "");
             return fansAsync.EndInvoke(fansResult);
         }
         
-        private string FansRequest()
-        {
-            var postParams = new Dictionary<string, string>()
-            {
+        private string FansRequest() {
+            var postParams = new Dictionary<string, string>() {
                 {"id", "14041725"}, // 书籍ID
                 {"pn", _requestPage.ToString()}, // 页码（无论如何，该接口最多只能获取总排行前500的用户数据，或月、周排行前50的数据）
                 {"ps", "50"}, // 每页数量，测试最多一次也只会返回50条
